be8739adcea05f63c8a31d8db3e4fb142de4cc21
sanchit903 <sanchitsinha903@gmail.com>
2025-05-05 23:51:09 +0530

    Refactor Firebase configuration to use environment variables for sensitive data

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyARquTjFANJgK2RvQSgbzF2xbWfPaKVMBM",
-    authDomain: "buybusy-1-64c5c.firebaseapp.com",
-    projectId: "buybusy-1-64c5c",
-    storageBucket: "buybusy-1-64c5c.firebasestorage.app",
-    messagingSenderId: "144003809186",
-    appId: "1:144003809186:web:dc6914bde98b7418db3cfb",
-    measurementId: "G-CYZKMKR2XX"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
